b977f12b8f7ac6684b60ce84e600241189f3863b
victor176S <105952113+victor176S@users.noreply.github.com>
2026-05-08 17:43:15 +0200

    Arreglo de un bug y añadido más sonidos

--- Assets/Scripts/AudioStorage.cs
+++ Assets/Scripts/AudioStorage.cs
@@ -27,6 +27,8 @@ public class AudioStorage : MonoBehaviour
         player = GameObject.FindWithTag("Player");
 
         movement = player.GetComponent<Movement>();
+
+        this.gameObject.transform.GetChild(0).gameObject.GetComponent<AudioSource>().volume = configManager.volumen;
     }
 
     // Update is called once per frame
@@ -46,6 +48,33 @@ public class AudioStorage : MonoBehaviour
         {
             Debug.Log("Sonido");
             this.gameObject.transform.GetChild(1).gameObject.GetComponent<AudioSource>().PlayOneShot(audios[0]);
+        }
+
+        if(player.transform.position.y > 200)
+        {
+
+            this.gameObject.transform.GetChild(0).gameObject.GetComponent<AudioSource>().volume = configManager.volumen / 200;
+
+            if(player.transform.position.y > 300)
+            this.gameObject.transform.GetChild(0).gameObject.GetComponent<AudioSource>().volume = configManager.volumen / 100;
+
+            if (!this.gameObject.transform.GetChild(0).gameObject.GetComponent<AudioSource>().isPlaying)
+            {
+
+                this.gameObject.transform.GetChild(0).gameObject.GetComponent<AudioSource>().clip = audios[1];
+                this.gameObject.transform.GetChild(0).gameObject.GetComponent<AudioSource>().loop = true;
+                this.gameObject.transform.GetChild(0).gameObject.GetComponent<AudioSource>().Play();
+
+            }
+
+            
+        }
+        else
+        {
+            this.gameObject.transform.GetChild(0).gameObject.GetComponent<AudioSource>().volume = configManager.volumen / 100;
+            this.gameObject.transform.GetChild(0).gameObject.GetComponent<AudioSource>().clip = null;
+            this.gameObject.transform.GetChild(0).gameObject.GetComponent<AudioSource>().loop = false;
+            this.gameObject.transform.GetChild(0).gameObject.GetComponent<AudioSource>().Stop();
         }
     }
 }
